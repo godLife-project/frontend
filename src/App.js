f16d146a2705b7ef8296c2a6cb049a0ecb220267
haeji1124 <haeji1124@naver.com>
2025-02-23 02:44:42 +0900

    shadcn-ui 적용용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React, {useEffect, useState} from 'react';
+import Button from "./components/ui/button";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+import Layout from './components/layout/Layout';
+import Home from './pages/Home/Home'
+
 
 function App() {
   return (
-      <></>
+    <div>
+      <h1>홈페이지</h1>
+      <Button>기본 버튼</Button>
+      <Button variant="outline">아웃라인 버튼</Button>
+      <Button variant="destructive">삭제 버튼</Button>
+    </div>
+    //  <Router>
+    //   <Layout>
+    //     <Routes>
+    //       <Route path="/" element={<Home/>}/>
+    //     </Routes>
+    //   </Layout>
+    //  </Router>
   );
 }
 
