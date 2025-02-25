6f9419701bacb8e19fe518f27ad703fd9d773cc8
haeji1124 <46364329+haeji1124@users.noreply.github.com>
2025-02-25 14:54:30 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Layout from './components/layout/Layout';
-import Home from './pages/Home/Home'
-import Signup from './pages/Auth/SignUp'
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastProvider } from "@/components/ui/use-toast";
+import { Toaster } from "@/components/ui/toaster";
+import Layout from "./components/layout/Layout";
+import Home from "./pages/Home/Home";
+import Signup from "./pages/Auth/SignUp";
+import Login from "./pages/Auth/Login";
 
 function App() {
   return (
-     <Router>
-      <Layout>
-        <Routes>
-          <Route path="/signup" element={<Signup/>}/>
-          <Route path="/" element={<Home/>}/>
-        </Routes>
-      </Layout>
-     </Router>
+    <ToastProvider>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Layout>
+        <Toaster />
+      </Router>
+    </ToastProvider>
   );
 }
-
 
 export default App;
