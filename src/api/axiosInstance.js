72f482d5a98470a5b74199095fef0a32e5a55ce6
rimmo <limcl1224@naver.com>
2025-05-31 00:50:43 +0900

    Feat: 검색 기능 추가

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 const axiosInstance = axios.create({
-  baseURL: "https://f4fd-182-229-89-82.ngrok-free.app/api", // Spring Boot 백엔드 API 주소
+  baseURL: "https://9e19-182-229-89-82.ngrok-free.app/api", // Spring Boot 백엔드 API 주소
   headers: {
     "ngrok-skip-browser-warning": "true",
     "Content-Type": "application/json; charset=UTF-8",
